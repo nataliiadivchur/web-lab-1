7a17bccfbf1a91ed30860d00d52169dbddd28842
nataliiadivchur <nataliia.divchur.pm.2019@lpnu.ua>
2021-03-22 14:20:58 +0200

    your changes

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,6 @@
 $(document).ready(function (){
 //$('body').hide()
 })
+$(document).on('click', 'wrapper sideBar ul li',function (){
+    $(this).addClass('active')
+})
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 const {src, dest, parallel, series, watch } = require('gulp'); //connection to gulp
-const browserSync = require('browser-sync').create();
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify-es').default;
-const autoprefixer=require('gulp-autoprefixer')
+const browserSync  = require('browser-sync').create();
+const concat       = require('gulp-concat');
+const uglify       = require('gulp-uglify-es').default;
+const sass         = require('node-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 function browser_sync(){ //function for connection to gulp
     browserSync.init({
@@ -23,18 +24,22 @@ function  scripts(){
         .pipe(browserSync.stream())
 }
 
-function buildcopy(){
+function buildcopy(){ //build function
     return src(['app/js/**/*.min.js',
     'app/**/*.html'
     ])
         .pipe(dest('dist'))
 }
 
-function start_watch(){
+function start_watch(){ //function for watching & reloading
     watch(['app/**/*.js','!app/**/*.min.js'],scripts) //to prevent recursion
+    watch('/**/*.html').on('change', browserSync.reload)
+    watch('/**/*/*.main.css').on('change',browserSync.reload)
 }
 
 exports.browser_sync = browser_sync;
 exports.scripts = scripts;
+
+
 exports.build = series(scripts, buildcopy);
 exports.default = parallel(scripts, browser_sync, start_watch );
